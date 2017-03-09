581eca07c984a729471bee00badb94eefab6df43
Decarpentrie <elodie.decarpentrie32@gmail.com>
2017-03-09 17:11:40 +0100

    rajout des var

--- app.js
+++ app.js
@@ -1,2 +1,40 @@
 var solution = Math.floor(Math.random() * 4);
-console.log(solution); 
+
+console.log(solution);
+
+var response = prompt("quel est le bon chiffre");
+
+console.log(response);
+
+if (solution = response) {
+	console.log ("gagner!");
+
+
+} else if (solution < response) {
+	console.log ("perdu");
+
+}else (solution > response);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
