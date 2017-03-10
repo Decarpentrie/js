cdcaf73f684960b3a8233f2b1e07a9e18e6a46b5
Decarpentrie <elodie.decarpentrie32@gmail.com>
2017-03-10 10:21:03 +0100

    rajout if, else if et else

--- app.js
+++ app.js
@@ -6,14 +6,32 @@ var response = prompt("quel est le bon chiffre");
 
 console.log(response);
 
-if (solution == response) {
+for (var response = 0; response < 3; response ++) {
+	
+	if (solution == response) {
 	console.log ("gagner!");
+	
 
+	
+	
 
 } else if (solution < response) {
 	console.log ("perdu");
+	prompt("perdu trop petit");
+	
 
-}else (solution > response);
+}else if(solution > response) {
+console.log ("perdu");
+	prompt("perdu trop grand ");
+	
+}
+	
+}
+
+
+
+
+
 
 
 
